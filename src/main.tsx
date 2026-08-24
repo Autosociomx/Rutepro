45efa17c0113874f5d2dff57ce9dc30becb9815c
Autosociomx <autosociomx@gmail.com>
2026-08-23 19:20:05 -0700

    feat(commerce): expose ConnectX public storefront

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,21 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ConnectXOSLab } from './components/ConnectXOSLab.tsx';
 import { ConnectXSetup } from './components/ConnectXSetup.tsx';
+import { ConnectXStorefront } from './components/ConnectXStorefront.tsx';
 import './index.css';
 
 const params = new URLSearchParams(window.location.search);
 const showConnectXOS = params.get('connectx_os') === '1';
 const showConnectXSetup = params.get('connectx_setup') === '1';
+const showConnectXStore = params.get('connectx_store') === '1';
 
 const root = showConnectXSetup
   ? <ConnectXSetup />
-  : showConnectXOS
-    ? <ConnectXOSLab />
-    : <App />;
+  : showConnectXStore
+    ? <ConnectXStorefront />
+    : showConnectXOS
+      ? <ConnectXOSLab />
+      : <App />;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{root}</StrictMode>,
